266f67656b50f83da627301781e9e14a60c3ff8f
Pranoto Budi <pranotobudi.s@gmail.com>
2021-12-13 09:27:22 +0700

    nhooyr-websocket chat & echo example

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/pranotobudi/websocket-apps
 
 go 1.16
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
---
+++ nhooyr-websocket/chat/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	log.SetFlags(0)
+
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//go run main.go chat.go localhost:8080
+}
+
+// run initializes the chatServer and then
+// starts a http.Server for the passed in address.
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("please provide an address to listen on as the first argument")
+	}
+
+	l, err := net.Listen("tcp", os.Args[1])
+	if err != nil {
+		return err
+	}
+	log.Printf("listening on http://%v", l.Addr())
+
+	cs := newChatServer()
+	s := &http.Server{
+		Handler:      cs,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+	errc := make(chan error, 1)
+	go func() {
+		errc <- s.Serve(l)
+	}()
+
+	// preparing signal from os like Ctrl+C
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+
+	// apps will terminate due to two condition: os signal or errc from s.Server(l)
+	// apps will wait here until something happen, because no "default" in this select.
+	select {
+	case err := <-errc:
+		log.Printf("failed to serve: %v", err)
+	case sig := <-sigs:
+		log.Printf("terminating: %v", sig)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	//even in it's official document function example, they don't call cancel(),
+	// so, I doubt it is a must.
+
+	return s.Shutdown(ctx)
+}
